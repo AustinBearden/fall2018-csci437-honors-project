9adbd0e705df1bc11fed2a4480c28923aa1b5bb9
aibearde <aibearde@iupui.edu>
2018-11-04 15:35:06 -0500

    Got set of screen panels and start of control panel

--- gameSpace/client.js
+++ gameSpace/client.js
@@ -2,6 +2,7 @@
 // If you want to modify your application's content, start in "index.js"
 
 import {ReactInstance} from 'react-360-web';
+import {Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,12 +11,114 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
+
+  // //construct a new Flat surface in my app runtime
+  // const myFlatSurface = new Surface(
+  //   300, /* width */
+  //   600, /* height */
+  //   Surface.SurfaceShape.Flat /* shape */
+  // );
+
+  // Render content1 to flat surface
+  const content1 = new Surface();
+  content1.setShape(Surface.SurfaceShape.Flat);
+  content1.resize(400, 400);
   r360.renderToSurface(
-    r360.createRoot('SimpleGame', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('contentOne', { /* initial props */ }),
+    content1
   );
 
+  content1.setAngle(
+    0,
+    0
+  );
+
+  // Render content2 to flat surface
+  const content2 = new Surface();
+  content2.setShape(Surface.SurfaceShape.Flat);
+  content2.resize(400, 400);
+  r360.renderToSurface(
+    r360.createRoot('contentTwo', { /* initial props */ }),
+    content2
+  );
+
+  content2.setAngle(
+    -Math.PI / 6, // horizontal angle
+    0 // vertical angle
+  );
+
+  // Render content3 to flat surface
+  const content3 = new Surface();
+  content3.setShape(Surface.SurfaceShape.Flat);
+  content3.resize(400, 400);
+  r360.renderToSurface(
+    r360.createRoot('contentThree', { /* initial props */ }),
+    content3
+  );
+
+  content3.setAngle(
+    +Math.PI / 6, // horizontal angle
+    0 // vertical angle
+  );
+
+  // Render content4 to flat surface
+  const content4 = new Surface();
+  content4.setShape(Surface.SurfaceShape.Flat);
+  content4.resize(400, 400);
+  r360.renderToSurface(
+    r360.createRoot('contentFour', { /* initial props */ }),
+    content4
+  );
+
+  content4.setAngle(
+    0, // horizontal angle
+    Math.PI / 8 // vertical angle
+  );
+
+  // Render content5 to flat surface
+  const content5 = new Surface();
+  content5.setShape(Surface.SurfaceShape.Flat);
+  content5.resize(400, 400);
+  r360.renderToSurface(
+    r360.createRoot('contentFive', { /* initial props */ }),
+    content5
+  );
+
+  content5.setAngle(
+    -Math.PI / 6, // horizontal angle
+    Math.PI / 8 // vertical angle
+  );
+  
+  // Render content6 to flat surface
+  const content6 = new Surface();
+  content6.setShape(Surface.SurfaceShape.Flat);
+  content6.resize(400, 400);
+  r360.renderToSurface(
+    r360.createRoot('contentSix', { /* initial props */ }),
+    content6
+  );
+
+  content6.setAngle(
+    +Math.PI / 6, // horizontal angle
+    Math.PI / 8 // vertical angle
+  );
+
+  // Render controlPanel to flat surface
+  const controlPanel = new Surface();
+  controlPanel.setShape(Surface.SurfaceShape.Flat);
+  controlPanel.resize(600, 300);
+  r360.renderToSurface(
+    r360.createRoot('controlPanel', { /* initial props */}),
+    controlPanel
+  );
+
+  controlPanel.setAngle(
+    0, // horizontal angle
+    -Math.PI / 6 // vertical angle
+
+  );
+
+  
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
 }
--- gameSpace/index.js
+++ gameSpace/index.js
@@ -1,47 +1,104 @@
 import React from 'react';
-import { Component } from 'react';
-import { WebView } from 'react-native';
 import {
   AppRegistry,
   StyleSheet,
-  Entity,
-  asset,
   Text,
   View,
+  Image,
+  asset,
 } from 'react-360';
 
-// export default class gameSpace extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.panel}>
-//         <View style={styles.greetingBox}>
-//           <Text style={styles.greeting}>
-//             Welcome to React 360
-//           </Text>
-//         </View>
-//       </View>
-//     );
-//   }
-// };
-
-var hello = "Hello You!!";
-
-export default class SimpleGame extends React.Component {
+export default class contentOne extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
-        <Text>Hello Everyone</Text>
+        <View style={styles.panel}>
+          <Image source={asset('CumminsOne.jpeg')} style={styles.panel}/>
+        </View>
+      </View>
+    );
+  }
+};
+
+class contentTwo extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.panel}>
+          <Image source={asset('CumminsTwo.jpeg')} style={styles.panel}/>
+        </View>
       </View>
     );
   }
 }
 
+class contentThree extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.panel}>
+          <Image source={asset('CumminsThree.jpeg')} style={styles.panel}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class contentFour extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.panel}>
+          <Image source={asset('CumminsFour.jpeg')} style={styles.panel}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class contentFive extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.panel}>
+          <Image source={asset('CumminsFive.jpeg')} style={styles.panel}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class contentSix extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.panel}>
+          <Image source={asset('CumminsSix.jpeg')} style={styles.panel}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class controlPanel extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>This is the control panel</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
 
   panel: {
     // Fill the entire surface
-    width: 1000,
-    height: 600,
+    width: 600,
+    height: 300,
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     justifyContent: 'center',
     alignItems: 'center',
@@ -57,4 +114,10 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('SimpleGame', () => SimpleGame);
+AppRegistry.registerComponent('contentOne', () => contentOne);
+AppRegistry.registerComponent('contentTwo', () => contentTwo);
+AppRegistry.registerComponent('contentThree', () => contentThree);
+AppRegistry.registerComponent('contentFour', () => contentFour);
+AppRegistry.registerComponent('contentFive', () => contentFive);
+AppRegistry.registerComponent('contentSix', () => contentSix);
+AppRegistry.registerComponent('controlPanel', () => controlPanel);
